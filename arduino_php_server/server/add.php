303b737e5621c6aa75bcabb24708330f1a04f7a1
5ran6 <5raan6@gmail.com>
2020-09-23 13:37:00 +0100

    Update add.php

--- arduino_php_server/server/add.php
+++ arduino_php_server/server/add.php
@@ -1,16 +1,18 @@
 <?php
-   	include("connect.php");
+   	//includes connect.php to handle connections
+	include("connect.php");
    	
    	$link=Connection();
 
 	$temp1=$_POST["temp1"];
 	$hum1=$_POST["hum1"];
-
+	//inserts values for temperature and humidity into tempLog Table
 	$query = "INSERT INTO `tempLog` (`temperature`, `humidity`) 
 		VALUES ('".$temp1."','".$hum1."')"; 
    	
    	mysql_query($query,$link);
 	mysql_close($link);
-
+	
+	//header function from connect.php
    	header("Location: index.php");
 ?>
